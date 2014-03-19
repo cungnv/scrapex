6972745c091ee5e26b10a00c01ce3754dae11217
Cung <cungjava2000@gmail.com>
2014-03-19 10:19:13 +0700

    a

--- http.py
+++ http.py
@@ -156,6 +156,8 @@ class DOM(Node):
 		return self.x("//input[@id='__VIEWSTATE']/@value").urlencode() or self.html().sub('__VIEWSTATE|','|').urlencode()
 	def aspx_ev(self):
 		return self.x("//input[@id='__EVENTVALIDATION']/@value").urlencode() or self.html().sub('__EVENTVALIDATION|','|').urlencode()
+	def aspx_prepage(self):
+		return self.x("//input[@id='__PREVIOUSPAGE']/@value").urlencode() or self.html().sub('__PREVIOUSPAGE|','|').urlencode()	
 
 
 
