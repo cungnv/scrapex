41254ee12cc6bac35b780d24b2d7bf58d1181902
Cung Nguyen <cungjava2000@gmail.com>
2017-02-22 16:26:03 +0700

    a

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
