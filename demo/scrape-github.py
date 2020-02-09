6a8b1e1edd4c15e7b952429e491c26909fd15534
Cung Nguyen <cungjava2000@gmail.com>
2020-02-09 16:13:01 +0700

    update

---
+++ demo/scrape-github.py
@@ -0,0 +1,16 @@
+
+from scrapex import Scraper, common
+
+s = Scraper(use_cache = True)
+
+doc = s.load('https://github.com/search?q=scraping')
+
+print(doc.extract("//title"))
+print(doc.extract("//h3[contains(text(),'results')]").strip())
+
+listings = doc.q("//ul[@class='repo-list']/li")
+
+print('number of listings on first page:', len(listings) )
+
+for listing in listings:
+	print('repo name: ',listing.extract(".//div[contains(@class,'text-normal')]/a"))
